b4e2c43fe26178e78ae4a4a2c15b4c742e384c19
Lakshmi Dhatri Gangavarapu <lakshmidhatri@Lakshmis-Air.Dlink>
2023-11-13 13:28:42 +0530

    Revert "Revert "added""
    This reverts commit ccb3471b5039ecec6e0d5884cb9f7237c0336061.

---
+++ main.py
@@ -0,0 +1,14 @@
+#write a function to calculate 2nd  power of number
+#return the result from the function and print the result
+def power (a:int):
+    return a ** 2
+    
+
+print(power(6))  
+
+#write a function to calculatr a ** n
+def pow(a:int ,n:int):
+    return a**n
+
+
+print(pow(2,5))
